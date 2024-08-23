e0d51894b674e60373b6c6f139614f1b1ee8d9f5
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2024-08-23 13:26:45 -0500

    fixing to work on pm2

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -8,14 +8,13 @@ shell.cp("-R", "src/services/calibre", "dist/services");
 // shell.mkdir("-p", "dist/public/books");
 shell.mkdir("-p", "dist/public");
 
-
-// const publicDir = path.join("dist/public", __dirname.slice(1), "dist/public");
-// shell.mkdir("-p", publicDir);
-// shell.ln("-s", path.join(__dirname, "dist/public/books"), path.join(publicDir, "books"));
+shell.cp("-R", "src/browser/*", "dist/public");
 
 shell.ln("-s", "/media/RIGO7/BACKUP/LIBROS", "dist/public/books");
 shell.ln("-s", "/media/RIGO7/Libretorio-conf/covers", "dist/public/covers");
 shell.ln("-s", "/media/RIGO7/Libretorio-conf/cache", "dist/public/cache");
 shell.ln("-s", "/media/RIGO7/Libretorio-conf/temp_covers", "dist/public/temp_covers");
 
-shell.cp("-R", "src/browser/*", "dist/public");
+const publicDir = path.join("dist/public", __dirname.slice(1), "dist/public");
+shell.mkdir("-p", publicDir);
+shell.ln("-s", path.join(__dirname, "dist/public/books"), path.join(publicDir, "books"));
