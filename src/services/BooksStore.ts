725d28835730d3f1062a610cfe3ba9d58fb2a164
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2024-11-18 10:23:42 -0500

    adding debug logs

--- src/services/BooksStore.ts
+++ src/services/BooksStore.ts
@@ -673,7 +673,9 @@ export class BooksStore {
 
 				let length = 0;
 				try {
-					const metadata: IAudioMetadata = await mm.parseFile(filePathResolved);
+					const metadata: IAudioMetadata = await mm.parseFile(
+						filePathResolved, {duration: true, skipCovers: true})
+					;
 					logger.info(`path: "${filePathResolved}" -  metadata: ${JSON.stringify(metadata)}`);
 					length = metadata.format.duration ?? 0;
 				} catch (error) {
